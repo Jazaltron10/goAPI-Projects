1a422dd18cdd9c378381442cc880d6aa16d95589
Jazaltron <jazaltron10@gmail.com>
2023-11-13 14:11:24 +0100

    First major project using the Echo framework

---
+++ echoAPI/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"net/http"
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	// e can be used to access everything in echo
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!, Here I am")
+	})
+	e.Logger.Fatal(e.Start(":1323"))
+}
