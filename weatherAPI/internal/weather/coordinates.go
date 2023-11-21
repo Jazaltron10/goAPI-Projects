5ad4211e728ce2085cda24627ec15e3c5bd1b594
Jazaltron <jazaltron10@gmail.com>
2023-11-21 12:17:49 +0100

    Restructuring 001

--- weatherAPI/internal/weather/coordinates.go
+++
@@ -1,8 +0,0 @@
-// internal/weather/coordinates.go
-package weather
-
-// Coordinates represents latitude and longitude of a location.
-type Coordinates struct {
-	Latitude  float64 `json:"lat"`
-	Longitude float64 `json:"lon"`
-}
